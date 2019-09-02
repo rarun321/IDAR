fe4cd229c0dfeb3703d37c4eb1480bc165a2df4e
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-02 15:01:50 -0700

    Removed a Section for Later

--- IDAR/ViewController.swift
+++ IDAR/ViewController.swift
@@ -67,7 +67,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let plane = PlaneNode(color: colorArray.randomElement()!, position: SCNVector3(0,0,0))
             let planeNode = plane.CreatePlaneNode()
             planeNode.addChildNode(TextNode(text: course.name!, position: SCNVector3(0.35,1.7,0) , font: UIFont.init(name: "Futura", size: 14)!).CreatetextNode())
-            planeNode.addChildNode(TextNode(text: course.enrollments![0].computed_current_grade!, position: SCNVector3(<#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>, <#T##z: CGFloat##CGFloat#>), font: <#T##UIFont#>))
+//            planeNode.addChildNode(TextNode(text: course.enrollments![0].computed_current_grade!, position: SCNVector3(<#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>, <#T##z: CGFloat##CGFloat#>), font: <#T##UIFont#>))
             container.addChildNode(planeNode)
         }
         
